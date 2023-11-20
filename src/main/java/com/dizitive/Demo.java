e2fc57cea2da0503bd3e47c9accb4208f8dd2db0
Ritesh <rk706473@gmail.com>
2023-11-20 15:18:11 +0530

    first

---
+++ src/main/java/com/dizitive/Demo.java
@@ -0,0 +1,11 @@
+package com.dizitive;
+
+import com.dizitive.beans.EncodeDecodeUtils;
+
+public class Demo {
+	public static void main(String[] args) {
+		EncodeDecodeUtils utils=new EncodeDecodeUtils();
+		System.out.println(utils.encode("Ritesh"));
+	}
+
+}
